bea6004238e023cf7402b3ffe13d0be90373f604
koreyama <yuuta.yama2003@gmail.com>
2026-01-07 18:03:24 +0900

    Fix releases page internal server error by removing edge runtime

--- src/app/releases/page.tsx
+++ src/app/releases/page.tsx
@@ -4,8 +4,6 @@ import React from 'react';
 import Link from 'next/link';
 import { IconBack } from '@/components/Icons';
 
-export const runtime = 'edge';
-
 const RELEASES = [
     {
         version: '2.0.0',
