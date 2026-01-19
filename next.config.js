cd42282721e98dc791e245e25fb7ffa3cb625823
koreyama <yuuta.yama2003@gmail.com>
2026-01-19 16:37:56 +0900

    Simplify build config and add debug log to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
+console.log("--- Loading next.config.js ---");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'standalone',
-    distDir: '.next',
 };
 
 module.exports = nextConfig;
