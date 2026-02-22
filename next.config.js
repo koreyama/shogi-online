39c6fbd22bf02d5783f3827dc5b7c5247add8532
koreyama <yuuta.yama2003@gmail.com>
2026-02-23 00:40:47 +0900

    fix(pwa): temporarily disable service worker to fix ERR_FAILED on mobile

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const withSerwist = require('@serwist/next').default({
     swSrc: 'src/app/sw.ts',
     swDest: 'public/sw.js',
     reloadOnOnline: true,
+    disable: true, // 緊急退避: Service Workerを無効化し登録を解除する
 });
 
 /** @type {import('next').NextConfig} */
